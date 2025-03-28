6f2e6ee2efc6c8427317259093b1f98d61312aa5
LUCAS RODRIGUES BEVILAQUA <97030780+LucasRB7@users.noreply.github.com>
2025-03-28 05:33:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+    async function send(){
+      let file = document.getElementById('file');
+      let img = file.files[0];
+      
+
+      if(!img){
+            alert('Selecione uma imagem para enviar!')
+      }else{
+        
+        const formData = new FormData();
+        formData.append('image', img);
+
+    try {
+        const response = await fetch('https://api.imgbb.com/1/upload?key=18c309babaac947633eaa732474c985b', {
+            method: 'POST',
+            body: formData,
+        });
+
+        const data = await response.json();
+        
+        if (data.status === 200) {
+            let url = data.data.url
+            console.log('Imagem enviada com sucesso:', url);
+            var unityIframe = document.getElementById('unity-iframe');
+            var unityInstance;
+            unityInstance = unityIframe.contentWindow.unityInstance;
+            sendTextureToUnity(url);
+            return;
+            
+        } else {
+            throw new Error('Falha no upload');
+        }
+    } catch (error) {
+        console.error('Erro no upload:', error);
+    }
+
+      }
+}
+function sendTextureToUnity(imageUrl) {
+    if (unityInstance) {
+        // Envia a URL para o Unity usando SendMessage
+        unityInstance.SendMessage('WebGl', 'ChangeTexture', imageUrl);
+        console.log("URL enviada para o Unity: " + imageUrl);
+    } else {
+        console.error("Unity WebGL não foi carregado corretamente no iframe.");
+        console.log(unityInstance)
+    }
+}
